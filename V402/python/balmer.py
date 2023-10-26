753a20fc701b3b0c2f3c2aea60b34b85e963cef0
Christian Fischer <chrfis2001@gmail.com>
2023-10-26 03:14:09 +0200

    Balmer

--- V402/python/balmer.py
+++ V402/python/balmer.py
@@ -43,6 +43,7 @@ alpha_hg = w_g_hg
 alpha_hg_err = w_g_hg_err
 beta_hg = w_b + w_g_hg - 180
 beta_hg_err = np.sqrt((w_g_hg_err)**2 + (w_b_err)**2)
+print('Ein- und Ausfallswinkel:', alpha_hg, alpha_hg_err, beta_hg, beta_hg_err)
 
 # in rad
 alpha_hg_rad = alpha_hg * (np.pi / 180)
@@ -54,33 +55,36 @@ beta_hg_rad_err = beta_hg_err * (np.pi / 180)
 g = lambda_hg / (np.sin(alpha_hg_rad) + np.sin(beta_hg_rad))
 g_err = (g / (np.sin(alpha_hg_rad) + np.sin(beta_hg_rad))) * np.sqrt((np.cos(alpha_hg_rad) * alpha_hg_rad_err)**2 + (np.cos(beta_hg_rad) * beta_hg_rad_err)**2)
 
-print(g , g_err)
+print('Gitterkonstante:', g, g_err)
 
 # Gitterkonstante aus Regressionsverfahren
 x_data_fit = lambda_hg[:10] * 10**(-9)
 y_data_fit = np.sin(alpha_hg_rad[:10]) + np.sin(beta_hg_rad[:10])
 y_data_fit_err = np.sqrt((np.cos(alpha_hg_rad[:10]) * alpha_hg_rad_err)**2 + (np.cos(beta_hg_rad[:10]) * beta_hg_rad_err)**2)
+print('y-Werte:', y_data_fit, y_data_fit_err)
 def gitterkonstante_fit(lam, a):
     return a * lam
 plt.errorbar(x_data_fit, y_data_fit, y_data_fit_err, fmt = 'o', markersize = 3, capsize = 3, label = 'Datenpunkte')
 popt1, pcov1 = curve_fit(gitterkonstante_fit, x_data_fit, y_data_fit)
-plt.plot(x_data_fit, gitterkonstante_fit(x_data_fit, *popt1), label = 'Fit: $a = (%5.3f \pm %5.3f)$ 1/m' % (popt1, pcov1[0, 0]**0.5))
+plt.plot(x_data_fit, gitterkonstante_fit(x_data_fit, *popt1), label = 'Anpassungsparameter: $a = (2455100 \pm 2800)$ 1/m')
 plt.xlabel('$\lambda$ / nm')
 labels = [400, 425, 450, 475, 500, 525, 550, 575]
 range = [400 * 10**(-9), 425 * 10**(-9), 450 * 10**(-9), 475 * 10**(-9), 500 * 10**(-9), 525 * 10**(-9), 550 * 10**(-9), 575 * 10**(-9)]
 plt.xticks(range, labels)
 plt.ylabel('$\sin(\\alpha) + \sin(\\beta)$ / dimensionslos')
 plt.legend()
+plt.grid()
 plt.savefig(f"../figs/gitterkonstante_fit.png", dpi = 400)
 
 # Chi2
 y_gitter_predicted = gitterkonstante_fit(x_data_fit, popt1)
 chi_squared_gitter = np.sum((y_data_fit - y_gitter_predicted)**2)
-print(chi_squared_gitter)
+print('chi2 Gitter:', chi_squared_gitter)
 
 # Gitterkonstante aus Fit
 gitterkonstante = 1/popt1
 gitterkonstante_err = (pcov1[0, 0]**0.5)/(popt1**2)
+print('Gitterkonstante aus Fit:', gitterkonstante, gitterkonstante_err)
 
 # Bestimmung der Balmerlinien
 
@@ -145,7 +149,8 @@ lambda_h_beta = 1/popt1 * (np.sin(h_beta_ein_rad) + np.sin(h_beta_aus_rad))
 lambda_h_beta_err = np.sqrt(((np.sin(h_beta_ein_rad) + np.sin(h_beta_aus_rad)) * gitterkonstante_err)**2 + (1/popt1 * np.cos(h_beta_ein_rad) * h_beta_ein_rad_err)**2 + (1/popt1 * np.cos(h_beta_aus_rad) * h_beta_aus_rad_err)**2)
 lambda_h_gamma = 1/popt1 * (np.sin(h_gamma_ein_rad) + np.sin(h_gamma_aus_rad))
 lambda_h_gamma_err = np.sqrt(((np.sin(h_gamma_ein_rad) + np.sin(h_gamma_aus_rad)) * gitterkonstante_err)**2 + (1/popt1 * np.cos(h_gamma_ein_rad) * h_gamma_ein_rad_err)**2 + (1/popt1 * np.cos(h_gamma_aus_rad) * h_gamma_aus_rad_err)**2)
-print(lambda_h_alpha, lambda_h_alpha_err, lambda_h_beta, lambda_h_beta_err, lambda_h_gamma, lambda_h_gamma_err)
+print('Ein- und Ausfallswinkel:', h_alpha_ein, h_alpha_aus, h_beta_ein, h_beta_aus, h_gamma_ein, h_gamma_aus)
+print('Wellenlängen:', lambda_h_alpha, lambda_h_alpha_err, lambda_h_beta, lambda_h_beta_err, lambda_h_gamma, lambda_h_gamma_err)
 
 # Isotopieaufspaltung
 delta_beta_h_alpha = h_alpha_isotopie_grad * (np.pi / 180) 
@@ -162,6 +167,8 @@ delta_lambda_h_beta_err = np.sqrt((np.cos(h_beta_aus_rad) * delta_beta_h_beta * 
 delta_lambda_h_gamma = gitterkonstante * np.cos(h_gamma_aus_rad) * delta_beta_h_gamma
 delta_lambda_h_gamma_err = np.sqrt((np.cos(h_gamma_aus_rad) * delta_beta_h_gamma * gitterkonstante_err)**2 + (gitterkonstante * np.sin(h_gamma_aus_rad) * delta_beta_h_gamma * h_gamma_aus_rad_err)**2 + (gitterkonstante * np.cos(h_gamma_aus_err) * delta_beta_h_gamma_err)**2)
 print(delta_lambda_h_alpha, delta_lambda_h_alpha_err, delta_lambda_h_beta, delta_lambda_h_beta_err, delta_lambda_h_gamma, delta_lambda_h_gamma_err)
+print('Winkelaufspaltung:', h_alpha_isotopie_grad, h_alpha_isotopie_grad_err, h_beta_isotopie_grad, h_beta_isotopie_grad_err)
+print('Isotopieaufspaltung:', delta_lambda_h_alpha, delta_lambda_h_alpha_err, delta_lambda_h_beta, delta_lambda_h_beta_err, delta_lambda_h_gamma, delta_lambda_h_gamma_err)
 
 # CCD-Kamera
 
@@ -171,7 +178,7 @@ x_data_rot = data_rot[:,0] + 0.080
 y_data_rot = data_rot[:,1]
 x_data_rot_err = 0.001
 y_data_rot_err = 0.1
-ind = np.array([i > -.02 and i < .043 for i in x_data_rot])
+ind_rot = np.array([i > -.0191 and i < .0401 for i in x_data_rot])
 
 # türkis
 data_türkis = np.loadtxt('../Daten/Turkis.txt', skiprows = 1)
@@ -179,38 +186,99 @@ x_data_türkis = data_türkis[:,0] - 0.075
 y_data_türkis = data_türkis[:,1]
 x_data_türkis_err = 0.001
 y_data_türkis_err = 0.1
+ind_türkis = np.array([i > -.02 and i < .03 for i in x_data_türkis])
+
 # lila
 data_lila = np.loadtxt('../Daten/Lila.txt', skiprows = 1)
 x_data_lila = data_lila[:,0] - 0.473
 y_data_lila = data_lila[:,1]
 x_data_lila_err = 0.001
 y_data_lila_err = 0.1
+ind_lila = np.array([i > -.05 and i < .05 for i in x_data_lila])
 
 #testfit
 def f(a, x):
-    return a[0] * np.exp(-((x - a[1])**2)/(a[2]**2)) + a[3] * np.exp(-((x - a[4])**2)/(a[5]**2)) + a[6]
+    return a[0] * np.exp(-((x - a[1])**2)/(2*a[2]**2)) + a[3] * np.exp(-((x - a[4])**2)/(2*a[5]**2)) + a[6]
 
 exp = Model(f)
-mydata = RealData(x_data_rot[ind], y_data_rot[ind], sx = x_data_rot_err, sy = y_data_rot_err)
-myodr = ODR(mydata, exp, beta0 = [80,0.0,0.01,10,0.03,0.02,6])
-myoutput = myodr.run()
-myoutput.pprint()
+mydata_rot = RealData(x_data_rot[ind_rot], y_data_rot[ind_rot], sx = x_data_rot_err, sy = y_data_rot_err)
+myodr_rot = ODR(mydata_rot, exp, beta0 = [80,0.0,0.01,10,0.03,0.01,6])
+myoutput_rot = myodr_rot.run()
+myoutput_rot.pprint()
 
-chi_square = chisquare(lambda x: f(myoutput.beta, x), x_data_rot, y_data_rot,
+mydata_türkis = RealData(x_data_türkis[ind_türkis], y_data_türkis[ind_türkis], sx = x_data_türkis_err, sy = y_data_türkis_err)
+myodr_türkis = ODR(mydata_türkis, exp, beta0 = [11,0.0,0.005,1,0.02,0.004,5.5])
+myoutput_türkis = myodr_türkis.run()
+myoutput_türkis.pprint()
+
+mydata_lila = RealData(x_data_lila[ind_lila], y_data_lila[ind_lila], sx = x_data_lila_err, sy = y_data_lila_err)
+myodr_lila = ODR(mydata_lila, exp, beta0 = [6,0.0,0.005,1,-0.4,0.01,6])
+myoutput_lila = myodr_lila.run()
+myoutput_lila.pprint()
+
+chi_square = chisquare(lambda x: f(myoutput_rot.beta, x), x_data_rot, y_data_rot,
                         np.array([y_data_rot_err]*len(x_data_rot)), 7)
 print(f"chi squared: {chi_square}")
 
 plt.subplots()
 plt.errorbar(x_data_rot, y_data_rot, xerr = x_data_rot_err, yerr = y_data_rot_err, linestyle = 'none', label = 'Datenpunkte')
-x = np.linspace(-.06, .075, 1000)
-plt.plot(x, f(myoutput.beta,x))
-plt.xlim([-0.06,0.075])
+x1 = np.linspace(-.02, .042, 1000)
+plt.plot(x1, f(myoutput_rot.beta, x1), label = 'Gauß-Anpassung')
+plt.xlim([-0.02,0.042])
+plt.legend()
+plt.title('rot')
+plt.xlabel('Winkel $\\beta$ / °')
+plt.ylabel('Intensität $I$ / %')
+plt.grid()
+plt.savefig(f"../figs/rot_fit.png", dpi = 400)
 
 plt.subplots()
 plt.errorbar(x_data_türkis, y_data_türkis, xerr = x_data_türkis_err, yerr = y_data_türkis_err, linestyle = 'none', label = 'Datenpunkte')
-plt.xlim([-0.035,0.04])
+x2 = np.linspace(-.02, .03, 1000)
+plt.plot(x2, f(myoutput_türkis.beta, x2), label = 'Gauß-Anpassung')
+plt.xlim([-0.02,0.03])
+plt.legend()
+plt.title('türkis')
+plt.xlabel('Winkel $\\beta$ / °')
+plt.ylabel('Intensität $I$ / %')
+plt.grid()
+plt.savefig(f"../figs/türkis_fit.png", dpi = 400)
 
 plt.subplots()
 plt.errorbar(x_data_lila, y_data_lila, xerr = x_data_lila_err, yerr = y_data_lila_err, linestyle = 'none', label = 'Datenpunkte')
-plt.xlim([-0.45,0.15])
-plt.show()
+x3 = np.linspace(-.05, .05, 1000)
+plt.plot(x3, f(myoutput_lila.beta, x3), label = 'Gauß-Anpassung')
+plt.xlim([-0.05,0.05])
+plt.legend()
+plt.title('violett')
+plt.xlabel('Winkel $\\beta$ / °')
+plt.ylabel('Intensität $I$ / %')
+plt.grid()
+plt.savefig(f"../figs/lila_fit.png", dpi = 400)
+
+# Bestimmung der Rydberg-Konstanten
+x_werte = np.array([(1/4 - 1/9), (1/4 - 1/16), (1/4 - 1/25)])
+y_werte = np.array([1540832, 2074688, 2336448])
+y_werte_err = np.array([11879, 25826, 32753])
+
+def rydbergkonstante_fit(x, R):
+    return R * x
+plt.subplots()
+plt.errorbar(x_werte, y_werte, y_werte_err, fmt = 'o', markersize = 3, capsize = 3, label = 'Datenpunkte')
+popt2, pcov2 = curve_fit(rydbergkonstante_fit, x_werte, y_werte)
+plt.plot(x_werte, rydbergkonstante_fit(x_werte, *popt2), label = 'Anpassungsparameter: $R_{\infty} = (11100000 \pm 20000)$ 1/m')
+plt.xlabel('$(1/4 - 1/n^2)$ / dimensionslos')
+plt.ylabel('$\\frac{1}{\lambda}$ / 1/m')
+plt.legend()
+plt.grid()
+plt.savefig(f"../figs/rydbergkonstante_fit.png", dpi = 400)
+
+# Chi2
+y_ryd_predicted = rydbergkonstante_fit(x_werte, popt2)
+chi_squared_ryd = np.sum((y_werte - y_ryd_predicted)**2)
+print('chi2 Ryd:', chi_squared_ryd)
+
+# Rydberg
+rydberg_konstante = popt2
+rydberg_konstante_err = pcov2[0, 0]**0.5
+print('Rydberg-Konstante:', rydberg_konstante, rydberg_konstante_err)
